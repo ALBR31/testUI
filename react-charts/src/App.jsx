d6621fe50d34ff29ce38c9686f90700b53cdb718
ALBR31 <118233821+ALBR31@users.noreply.github.com>
2023-11-03 09:55:39 -0300

    Add files via upload

---
+++ react-charts/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import HighChartsContent from './components/HighChartsContent';
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <HighChartsContent />
+    </>
+
+  );
+}
+
+export default App;
